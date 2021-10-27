ecbe3064111e45a7092e1c8750219f6246ebe553
piere.moawad <piere.moawad@gmail.com>
2021-10-27 11:38:08 +0200

    Moved api logic to UserDetailsService - Added custom authenticator for api endpoint

--- src/main/java/com/pierre/oceanblu/controller/ApiController.java
+++ src/main/java/com/pierre/oceanblu/controller/ApiController.java
@@ -3,20 +3,14 @@ package com.pierre.oceanblu.controller;
 import com.pierre.oceanblu.model.AuthenticationRequest;
 import com.pierre.oceanblu.model.AuthenticationResponse;
 import com.pierre.oceanblu.model.Transaction;
-import com.pierre.oceanblu.security.JwtUtil;
 import com.pierre.oceanblu.service.MainService;
 import com.pierre.oceanblu.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,33 +18,20 @@ import java.util.stream.Collectors;
 public class ApiController {
 
     private final MainService mainService;
-    private final AuthenticationManager authenticationManager;
     private final MyUserDetailsService userDetailsService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> createAuthenticationToken(
-            @RequestBody AuthenticationRequest request) throws Exception {
+            @RequestBody AuthenticationRequest request) {
 
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
-            );
-        } catch (BadCredentialsException e) {
-
-            throw new Exception(e);
-        }
-
-        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
-        String jwt = jwtUtil.generateToken(userDetails);
-
+        String jwt = userDetailsService.authenticateUserAndGetJwt(request.getUsername(), request.getPassword());
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
     @GetMapping("/stock")
-    public ResponseEntity<List<String>> getStock() {
+    public ResponseEntity<String[]> getStock() {
 
         List<Transaction> transactions = mainService.listAllTransactions();
-        return ResponseEntity.ok(transactions.stream().map(Objects::toString).collect(Collectors.toList()));
+        return ResponseEntity.ok(transactions.stream().map(Objects::toString).toArray(String[]::new));
     }
 }
